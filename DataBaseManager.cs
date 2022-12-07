49f52827f738307b461594b4344b7584ddef2a96
Brandon Johnson <77591826+brandon121j@users.noreply.github.com>
2022-12-07 17:30:51 -0500

    Update DataBaseManager.cs

--- DataBaseManager.cs
+++ DataBaseManager.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ExcelComparer
 {
     public class DatabaseManager
